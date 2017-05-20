6e415b9e714fe52c4db52362ce85594742c0894d
Owen Buckley <owen.buckley@analogstudios.net>
2017-05-20 16:15:54 -0400

    copy update

--- src/views/about/about.tsx
+++ src/views/about/about.tsx
@@ -13,12 +13,12 @@ export default class About extends React.Component<{}, {}> {
           <div className='col-xs-8'>
             <p className='welcome-text-body'>The Greenhouse is a developer platform for open source
               projects looking to get hosted, developed and incubated.  Run by Owen Buckley, who
-              brings a passion to the development process in the realization of an idea as a package
-              of connected information, turned into a meaningful product.  With knowledge and
-              experience in all stages of the development lifecycle (in particular for front-end
-              applications) as well as consulting, presenting at conferences and Meetups, advising
-              on technical and business strategy, and being active in Open Source, the Greenhouse
-              is a head in the clouds.</p>
+              brings a passion to the development process from the perspective of realizing that an
+              idea is a package of connected information and being able to turn that into a
+              meaningful product.  With knowledge and experience in all stages of the development
+              lifecycle (in particular for front-end applications) as well as consulting, presenting
+              at conferences and Meetups, advising on technical and business strategy,
+              and being active in Open Source, the Greenhouse is ahead in the clouds.</p>
           </div>
 
           <div className='col-xs-2'>
